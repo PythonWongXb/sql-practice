38addcc8975f6daf89ae3f35a8e81e806df31f7e
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-06 11:37:30 +0800

    Update 20230606.sql

--- sqls/20230606.sql
+++ sqls/20230606.sql
@@ -24,4 +24,7 @@ select * from sql_store.customers
 # order by
 select * from sql_store.customers
     order by state, last_name;
-
+    
+select *, quantity * unit_price as total_price from sql_store.order_items
+    where order_id = '2'
+    order by total_price desc;
