5f6e0e808c9e7cd2d564c3724b0826764dc96d7f
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-06 11:33:35 +0800

    Create 20230606.sql

---
+++ sqls/20230606.sql
@@ -0,0 +1,27 @@
+select * from sql_store.customers;
+select points, city, count(*) as num from sql_store.customers
+group by points, city
+order by points DESC ;
+# IN
+select * from sql_store.customers where state not in ('GA', 'CO');
+# between
+select * from sql_store.customers where birth_date between '1986-03-28' and '1992-05-23';
+# like
+# - %: any
+# - _: one operator
+select * from sql_store.customers where customers.last_name like '%b%';
+select * from sql_store.customers where customers.last_name like 'b____y';
+select * from sql_store.customers
+         where (customers.address like '%Trail%' or customers.address like '%Avenue%')
+         and customers.phone like '%9';
+
+# regexp
+select * from sql_store.customers
+    where last_name regexp '^b|c|l$';
+select * from sql_store.customers
+    where last_name regexp '[gim]e';
+
+# order by
+select * from sql_store.customers
+    order by state, last_name;
+
