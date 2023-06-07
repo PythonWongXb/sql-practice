d40ed1dd2803d3272747adc16378a82729657a49
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-07 10:28:10 +0800

    Update 20230607.sql

--- sqls/20230607.sql
+++ sqls/20230607.sql
@@ -1,1 +1,13 @@
+-- cross db join tb
+use sql_store;
+select order_id, oi.product_id, quantity, oi.unit_price, p.unit_price from order_items oi
+    join sql_inventory.products p
+    on p.product_id = oi.product_id
+    
+-- self join
+use sql_hr;
+select e.employee_id, e.first_name, m.first_name as manage_first_name from employees e
+    join employees m on e.reports_to = m.employee_id;
 
+select e.employee_id, e.first_name from employees e
+    where e.reports_to is null;
