c376804db7dfe5a362112b6cd5a196b3a5a36c9d
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-07 17:33:30 +0800

    Update 20230607.sql

--- sqls/20230607.sql
+++ sqls/20230607.sql
@@ -12,3 +12,15 @@ select e.employee_id, e.first_name, m.first_name as manage_first_name from emplo
 select e.employee_id, e.first_name from employees e
     where e.reports_to is null;
 
+use sql_invoicing;
+select c.client_id, c.name, c.city, pm.name as pay_method, p.date, p.amount
+from payments p
+join clients c on c.client_id = p.client_id
+join payment_methods pm on p.payment_method = pm.payment_method_id
+
+-- combine primary key join table
+select oi.*, note
+from sql_store.order_items oi
+join sql_store.order_item_notes oin
+    on oin.order_id = oi.order_id
+    and oin.product_id = oi.product_id;
