bc036e27fe4e923feb16b0a30af564620329c901
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-08 20:41:08 +0800

    Update 20230608.sql
    update single row

--- sqls/20230608.sql
+++ sqls/20230608.sql
@@ -38,3 +38,51 @@ insert into shippers (name)
         ('shipper3');
 
 select * from shippers;
+
+-- insert 
+insert into orders (customer_id, order_date, status)
+    values
+        (1, '2023-06-08', 1);
+
+select last_insert_id();
+insert into order_items
+    values
+        (last_insert_id(), 1, 1, 2.2),
+        (last_insert_id(), 2, 2, 3.2);
+
+select * from products;
+
+-- copy all
+create table order_archived as
+select * from orders;
+
+-- copy part
+insert into order_archived
+select * from orders
+where order_date < '2022-01-01';
+
+-- copy test
+use sql_invoicing;
+create table invoices_archived as
+select i.*, c.name as client_name
+from invoices i
+left join clients c using(client_id)
+where payment_date is not null;
+
+-- update single row
+update invoices
+set payment_date = '2023-01-22', payment_total = 10.2
+where invoice_id = 1;
+
+update invoices
+set payment_date = null, payment_total = default
+where invoice_id = 1;
+
+update invoices
+set payment_date = due_date, payment_total = invoice_total * 0.5
+where invoice_id = 3;
+
+select *
+from invoices;
+
+
