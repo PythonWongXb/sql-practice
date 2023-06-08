a071a16a8eef025cc2056527bb32b04465a5a02a
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-08 10:51:15 +0800

    Update 20230608.sql
    using

--- sqls/20230608.sql
+++ sqls/20230608.sql
@@ -1,1 +1,11 @@
+-- using
+use sql_invoicing;
 
+select date, c.name as client, amount, pm.name, i.number as invoiceNum
+from payments p
+left join clients c
+    using (client_id)
+left join invoices i
+    using (invoice_id)
+left join payment_methods pm
+    on p.payment_method = pm.payment_method_id
