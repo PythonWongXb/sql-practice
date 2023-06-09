6881803a52848cdb1411cdc776bf8f976e2e3315
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-09 16:08:45 +0800

    Update 20230609.sql

--- sqls/20230609.sql
+++ sqls/20230609.sql
@@ -32,3 +32,41 @@ where s.shipper_id in (
     select shipper_id from orders o
     where o.comments in ('demo')
     )
+  
+-- aggregate functions
+select
+    max(i.payment_total) as max,
+    min(i.payment_total) as min,
+    avg(i.payment_total) as avg,
+    sum(i.payment_total) as sum,
+    count(*) as count,
+    count(i.payment_date) as not_full_count,
+    count(distinct client_id) as disint_count
+from sql_invoicing.invoices i
+where i.payment_date > '1990-01-01';
+
+select
+    'before half 2019' as date_range,
+    sum(i.invoice_total) as total_sales,
+    sum(i.payment_total) as total_payments,
+    sum(i.invoice_total - i.payment_total) as what_we_expect
+from sql_invoicing.invoices i
+where i.payment_date between '2019-01-01' and '2019-06-30'
+union
+select
+    'after half 2019' as date_range,
+    sum(i.invoice_total) as total_sales,
+    sum(i.payment_total) as total_payments,
+    sum(i.invoice_total - i.payment_total) as what_we_expect
+from sql_invoicing.invoices i
+where i.payment_date between '2019-07-01' and '2019-12-31'
+union
+select
+    'total 2019' as date_range,
+    sum(i.invoice_total) as total_sales,
+    sum(i.payment_total) as total_payments,
+    sum(i.invoice_total - i.payment_total) as what_we_expect
+from sql_invoicing.invoices i
+where i.payment_date between '2019-01-01' and '2019-12-31';
+
+select * from sql_invoicing.invoices
