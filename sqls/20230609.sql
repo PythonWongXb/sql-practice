b5d2165210ba607780bb6d17ab4903bb9e37cf3a
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-09 11:18:11 +0800

    Update 20230609.sql

--- sqls/20230609.sql
+++ sqls/20230609.sql
@@ -24,3 +24,11 @@ where customer_id in (
 select * from orders o
     left join customers using(customer_id)
 where points > 3000;
+
+-- del
+set foreign_key_checks = 0;
+delete from shippers s
+where s.shipper_id in (
+    select shipper_id from orders o
+    where o.comments in ('demo')
+    )
