0523f56e1ba0b93fbf92f8075c4f0a865f69d18f
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-09 11:09:00 +0800

    multiple update and sub-query

--- sqls/20230609.sql
+++ sqls/20230609.sql
@@ -1,1 +1,26 @@
+-- multiple update and sub-query
 
+use sql_invoicing;
+update invoices
+set payment_date = '2023-02-02'
+where client_id in
+    (select client_id
+        from clients c
+        where c.name in ('Vinte', 'Myworks')
+    );
+
+select client_id
+        from clients c
+        where c.name in ('Vinte', 'Myworks')
+
+use sql_store;
+update orders o
+set comments = 'gold customer'
+where customer_id in (
+    select customer_id from customers c
+    where points > 3000
+    ) and comments is null ;
+
+select * from orders o
+    left join customers using(customer_id)
+where points > 3000;
