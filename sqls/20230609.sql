a6daf39c3f210d124cedaf67cd7f1c6f9faba2d8
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-10 21:30:32 +0800

    Update 20230609.sql

--- sqls/20230609.sql
+++ sqls/20230609.sql
@@ -70,3 +70,69 @@ from sql_invoicing.invoices i
 where i.payment_date between '2019-01-01' and '2019-12-31';
 
 select * from sql_invoicing.invoices
+
+-- group by
+select sum(i.invoice_total) as sale_total, c.name as userName, city
+from sql_invoicing.invoices i
+left join sql_invoicing.clients c using (client_id)
+where i.invoice_date > '2019-01-07'
+group by client_id, client_id
+order by sale_total desc;
+
+select date, c.name as clientName, pc.name as payMethod, amount
+from sql_invoicing.payments p
+left join sql_invoicing.payment_methods pc
+    on pc.payment_method_id = p.payment_method
+left join sql_invoicing.clients c
+    using (client_id)
+where p.date > '2019-01-01'
+group by date, client_id, pc.name, amount
+order by date;
+
+select distinct date, count(*) from sql_invoicing.payments
+group by date;
+
+select date, amount from sql_invoicing.payments
+
+-- where and having
+select c.name as client_name, sum(p.amount) as total_pay, c.city
+from sql_invoicing.payments p
+join sql_invoicing.clients c using (client_id)
+group by client_name, c.city
+having total_pay > 100
+
+-- sub queries
+select *
+from employees e
+where salary > (
+    select avg(salary) from employees
+    )
+order by salary;
+
+select avg(salary) from employees;
+
+-- in operator
+select c.name
+from sql_invoicing.clients c
+where client_id not in (
+    select distinct client_id
+    from sql_invoicing.invoices
+);
+
+-- join vs sub queries
+use sql_store;
+-- join better in this
+select distinct customer_id, c.first_name, c.last_name
+from orders o
+left join order_items oi using(order_id)
+left join customers c using(customer_id)
+where product_id = 3;
+-- sub queries
+select customer_id, c.first_name, c.last_name
+from customers c
+where c.customer_id in (
+    select o.customer_id
+    from orders o
+    left join order_items oi using(order_id)
+    where product_id = 3
+)
