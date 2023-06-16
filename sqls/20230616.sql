ae0c65b190c4dd5cfc1bf58e46c73d3a853faee9
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-16 09:40:55 +0800

    Update 20230616.sql
    params procedure

--- sqls/20230616.sql
+++ sqls/20230616.sql
@@ -1,1 +1,9 @@
+drop procedure if exists get_invoice_by_client_id;
+create procedure get_invoice_by_client_id(client_id int)
+    begin
+        select *
+        from invoices i
+        where i.client_id = client_id;
+    end;
 
+call get_invoice_by_client_id(3)
