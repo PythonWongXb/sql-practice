2bec423c62d977610380bf3985ac2c4295dc351f
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-16 15:36:40 +0800

    Update 20230616.sql
    param validator

--- sqls/20230616.sql
+++ sqls/20230616.sql
@@ -24,3 +24,25 @@ create procedure get_payments(client_id int, payment_method_id tinyint)
 call get_payments(null, null);
 call get_payments(1, null);
 call get_payments(5, 1);
+
+
+drop procedure if exists set_payment;
+create procedure set_payment(
+    invoice_id int,
+    payment_total decimal(9, 2),
+    payment_date date
+)
+    begin
+        if payment_total < 0
+            then signal sqlstate '12200' set message_text = 'whatever';
+        end if;
+        update invoices i
+            set
+                i.payment_total = ifnull(payment_total, i.payment_total),
+                i.payment_date = ifnull(payment_date, i.payment_date)
+        where i.invoice_id = invoice_id;
+    end;
+
+call set_payment(2, 102, '2023-10-02');
+call set_payment(2, -102, '2023-10-02');
+
