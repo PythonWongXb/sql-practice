40b1662bbe386f239dae4f07fc65a5386f123238
Evan Zuo <49554656+PythonWongXb@users.noreply.github.com>
2023-06-16 10:05:40 +0800

    Update 20230616.sql

--- sqls/20230616.sql
+++ sqls/20230616.sql
@@ -7,3 +7,20 @@ create procedure get_invoice_by_client_id(client_id int)
     end;
 
 call get_invoice_by_client_id(3)
+
+
+drop procedure if exists get_payments;
+create procedure get_payments(client_id int, payment_method_id tinyint)
+    begin
+        select p.invoice_id, c.client_id, c.name, pm.name
+        from payments p
+        left join payment_methods pm on pm.payment_method_id = p.payment_method
+        left join clients c on p.client_id = c.client_id
+        where
+            p.client_id = ifnull(client_id, p.client_id)
+            and pm.payment_method_id = ifnull(payment_method_id, pm.payment_method_id);
+    end;
+
+call get_payments(null, null);
+call get_payments(1, null);
+call get_payments(5, 1);
